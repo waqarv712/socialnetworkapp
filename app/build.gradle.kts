43eaf56eb460e3102ad9f8af378d332637618a25
SYSTEMS\mwaqar.akram <mwaqar.akram@systemsltd.com>
2023-08-28 19:27:12 +0500

    - Parcelize plugin added - compile sdk version updated - junit jupiter and some other dependencies added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,13 @@
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-parcelize")
 }
 
 android {
     namespace = "com.waqarvicky.socialnetworkapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.waqarvicky.socialnetworkapp"
@@ -51,7 +53,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -66,4 +68,38 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+
+    implementation("androidx.navigation:navigation-compose:2.7.1")
+    implementation("io.insert-koin:koin-android:3.1.4")
+    implementation("io.insert-koin:koin-androidx-compose:3.1.4")
+    implementation("com.google.accompanist:accompanist-swiperefresh:0.24.1-alpha")
+
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation("androidx.test.ext:junit-ktx:1.1.5")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.2")
+
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+
+}
+
+tasks.withType<JavaCompile> {
+    options.compilerArgs.addAll(listOfNotNull(
+        "--enable-preview",
+        "--add-modules", "jdk.incubator.concurrent"
+    ))
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+
+    jvmArgs("--enable-preview", "--add-modules", "jdk.incubator.concurrent")
+
+    testLogging {
+        events("passed", "skipped", "failed", "standardOut", "standardError")
+    }
 }
