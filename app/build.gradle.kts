f37424cf64bcebecf9f28df78fd9ea4e9713e2e4
SYSTEMS\mwaqar.akram <mwaqar.akram@systemsltd.com>
2023-08-30 19:05:11 +0500

    Sign up screen fields interactions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
--- app/src/main/java/com/waqarvicky/socialnetworkapp/signup/SignUp.kt
+++ app/src/main/java/com/waqarvicky/socialnetworkapp/signup/SignUp.kt
@@ -6,8 +6,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -15,10 +20,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -72,7 +81,7 @@ fun SignUp() {
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
-private fun PasswordField(
+private fun EmailField(
     value: String,
     onValueChange: (String) -> Unit
 ) {
@@ -80,24 +89,38 @@ private fun PasswordField(
         modifier = Modifier.fillMaxWidth(),
         value = value,
         label = {
-            Text(text = stringResource(id = R.string.password))
+            Text(text = stringResource(id = R.string.email))
         },
-        onValueChange = {onValueChange})
+        onValueChange = onValueChange )
 }
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
-private fun EmailField(
+private fun PasswordField(
     value: String,
     onValueChange: (String) -> Unit
 ) {
+    var passwordVisible by rememberSaveable { mutableStateOf(false) }
+
     OutlinedTextField(
         modifier = Modifier.fillMaxWidth(),
-        value = "",
+        value = value,
         label = {
-            Text(text = stringResource(id = R.string.email))
+            Text(text = stringResource(id = R.string.password))
         },
-        onValueChange = { onValueChange })
+        visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+        trailingIcon = {
+            val image = if (passwordVisible)
+                Icons.Filled.Visibility
+            else Icons.Filled.VisibilityOff
+            // Please provide localized description for accessibility services
+            val description = if (passwordVisible) "Hide password" else "Show password"
+
+            IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                Icon(imageVector = image, description)
+            }
+        },
+        onValueChange = onValueChange)
 }
 
 @Composable
