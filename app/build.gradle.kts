218ad662880d31b6deb7121ac0d1507c35af1b08
SYSTEMS\mwaqar.akram <mwaqar.akram@systemsltd.com>
2023-08-30 19:33:37 +0500

    Setup for junit 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,8 +78,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.ext:junit-ktx:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
@@ -91,9 +90,6 @@ dependencies {
     implementation("io.insert-koin:koin-android:3.1.4")
     implementation("io.insert-koin:koin-androidx-compose:3.1.4")
     implementation("com.google.accompanist:accompanist-swiperefresh:0.24.1-alpha")
-
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    androidTestImplementation("androidx.test.ext:junit-ktx:1.1.5")
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
