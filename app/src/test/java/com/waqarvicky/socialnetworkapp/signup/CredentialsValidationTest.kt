d20b1c57af4274ac87b9579a3fc1b027e070a632
SYSTEMS\mwaqar.akram <mwaqar.akram@systemsltd.com>
2023-08-31 16:01:10 +0500

    Can detect different invalid email formats

--- app/src/test/java/com/waqarvicky/socialnetworkapp/signup/CredentialsValidationTest.kt
+++ app/src/test/java/com/waqarvicky/socialnetworkapp/signup/CredentialsValidationTest.kt
@@ -5,15 +5,42 @@ import com.waqarvicky.socialnetworkapp.signup.state.SignUpState
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.EmptySource
+import org.junit.jupiter.params.provider.NullAndEmptySource
+import org.junit.jupiter.params.provider.NullSource
+import org.junit.jupiter.params.provider.ValueSource
 
 @ExtendWith(InstantTaskExecutorExtension::class)
 class CredentialsValidationTest {
 
-    @Test
-    fun invalidEmail() {
+    @ParameterizedTest
+    /*@EmptySource
+    @NullSource
+    @NullAndEmptySource
+    @ValueSource(
+        strings = [
+            "email",
+            "a@b.c",
+            "ab@b.c",
+            "ab@bc.c",
+            "",
+            "      "
+        ]
+    )*/
+    @CsvSource(
+        "'email'",
+        "'a@b.c'",
+        "'ab@b.c'",
+        "'ab@bc.c'",
+        "''",
+        "'      '",
+    )
+    fun invalidEmail(email: String) {
 
         val viewModel = SignUpViewModel()
-        viewModel.createAccount("email", ":password:", ":about:")
+        viewModel.createAccount(email, ":password:", ":about:")
 
         assertEquals(SignUpState.BadEmail, viewModel.signUpState.value)
     }
