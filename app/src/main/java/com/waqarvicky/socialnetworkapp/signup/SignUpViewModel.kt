64a9cfcfcc2ee82f2c989256e2448cff595d5226
SYSTEMS\mwaqar.akram <mwaqar.akram@systemsltd.com>
2023-09-01 16:56:19 +0500

    Separated the state assigning and the delivery

--- app/src/main/java/com/waqarvicky/socialnetworkapp/signup/SignUpViewModel.kt
+++ app/src/main/java/com/waqarvicky/socialnetworkapp/signup/SignUpViewModel.kt
@@ -16,15 +16,16 @@ class SignUpViewModel {
     ) {
         val emailRegx = """[a-zA-Z0-9+._%\-]{1,64}@[a-zA-Z0-9][a-zA-Z0-9\-]{1,64}(\.[a-zA-Z0-9][a-zA-Z0-9\-]{1,25})"""
         val emailPattern = Pattern.compile(emailRegx)
-
         val passwordRegx = """^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*+=\-]).{8,}$"""
         val passwordPattern = Pattern.compile(passwordRegx)
 
-        if (!emailPattern.matcher(email).matches()){
-            _mutableSignUpState.value = SignUpState.BadEmail
+        val state = if (!emailPattern.matcher(email).matches()){
+            SignUpState.BadEmail
         }else if (!passwordPattern.matcher(password).matches()){
-            _mutableSignUpState.value = SignUpState.BadPassword
-        }
+            SignUpState.BadPassword
+        }else TODO()
+
+        _mutableSignUpState.value = state
     }
 
 
