7c9208dc0a8c7dbbd3d16aa14e4535baa207cb6c
SYSTEMS\mwaqar.akram <mwaqar.akram@systemsltd.com>
2023-09-05 17:06:54 +0500

    Can open timeline upon successful signup

--- app/src/main/java/com/waqarvicky/socialnetworkapp/MainActivity.kt
+++ app/src/main/java/com/waqarvicky/socialnetworkapp/MainActivity.kt
@@ -9,7 +9,11 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.waqarvicky.socialnetworkapp.signup.SignUp
 import com.waqarvicky.socialnetworkapp.ui.theme.SocialNetworkAppTheme
 
@@ -17,14 +21,20 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
             SocialNetworkAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    SignUp{
-                        //navigate to timeline
+                    NavHost(navController = navController, startDestination = "signUp"){
+                        composable("signUp"){
+                            SignUp(onsignedUp = {navController.navigate("timeline")})
+                        }
+                        composable("timeline"){
+                            Text(text = stringResource(id = R.string.timeLine))
+                        }
                     }
                 }
             }
